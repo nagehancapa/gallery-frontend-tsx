47f646efae03c3a740d5b59629ecb8ecd51b1f61
nagehancapa <capanagehan@gmail.com>
2021-11-11 15:45:56 +0100

    fixed api url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const apiUrl =
-  process.env.API_URL || "http://artgallery-server.herokuapp.com/";
+  process.env.API_URL || "https://artgallery-server.herokuapp.com/";
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
